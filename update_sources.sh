4a09b1dbf8dbbccab2d8e8fc651bd322bc7d1646
Sinval Silva Junior <54493316+kjunda01@users.noreply.github.com>
2024-04-25 20:48:47 -0300

    Update update_sources.sh

--- update_sources.sh
+++ update_sources.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Defina os novos dados do sources.list
+new_sources="
+deb http://192.168.1.111:3142/archive.ubuntu.com/ubuntu jammy main universe multiverse
+deb http://192.168.1.111:3142/archive.ubuntu.com/ubuntu jammy-updates main universe multiverse
+deb http://192.168.1.111:3142/security.ubuntu.com/ubuntu jammy-security main universe multiverse
+"
+
+# Salve os novos dados em um arquivo temporário
+echo "$new_sources" > /tmp/new_sources.list
+
 # Substitua o arquivo sources.list
-sudo cp /tmp/apt-cache/sources.list /etc/apt/sources.list
+sudo cp /tmp/new_sources.list /etc/apt/sources.list
 
 # Limpeza
-rm -rf /tmp/apt-cache
+rm /tmp/new_sources.list
 
 echo "Arquivo sources.list atualizado com sucesso!"
