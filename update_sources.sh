7e63c775dc8b43c8ef2530e6b943486e0f868808
Sinval Silva Junior <54493316+kjunda01@users.noreply.github.com>
2024-04-25 23:58:31 -0300

    Update update_sources.sh

--- update_sources.sh
+++ update_sources.sh
@@ -2,9 +2,26 @@
 
 # Defina os novos dados do sources.list
 new_sources="
-deb http://192.168.1.111:3142/archive.ubuntu.com/ubuntu jammy main universe multiverse
-deb http://192.168.1.111:3142/archive.ubuntu.com/ubuntu jammy-updates main universe multiverse
-deb http://192.168.1.111:3142/security.ubuntu.com/ubuntu jammy-security main universe multiverse
+eb http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy main restricted
+deb-src http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy main restricted
+deb http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy-updates main restricted
+deb-src http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy-updates main restricted
+deb http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy universe
+deb-src http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy universe
+deb http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy-updates universe
+deb-src http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy-updates universe
+deb http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy multiverse
+deb-src http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy multiverse
+deb http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy-updates multiverse
+deb-src http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy-updates multiverse
+deb http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy-backports main restricted universe multiverse
+deb-src http://192.168.1.199:3142/archive.ubuntu.com/ubuntu/ jammy-backports main restricted universe multiverse
+deb http://192.168.1.199:3142/security.ubuntu.com/ubuntu jammy-security main restricted
+deb-src http://192.168.1.199:3142/security.ubuntu.com/ubuntu jammy-security main restricted
+deb http://192.168.1.199:3142/security.ubuntu.com/ubuntu jammy-security universe
+deb-src http://192.168.1.199:3142/security.ubuntu.com/ubuntu jammy-security universe
+deb http://192.168.1.199:3142/security.ubuntu.com/ubuntu jammy-security multiverse
+deb-src http://192.168.1.199:3142/security.ubuntu.com/ubuntu jammy-security multiverse
 "
 
 # Salve os novos dados em um arquivo temporário
